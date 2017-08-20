0656e5e5992eacdb1504b40ce83cf3866fadd93e
yunyu950908 <yunyu950908@gmail.com>
2017-08-21 05:49:22 +0800

    React.render

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
 ReactDOM.render(
     <h1>Hello React!</h1>,
     document.getElementById("root")
 )
+
