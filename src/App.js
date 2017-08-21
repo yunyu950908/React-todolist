cdb92c03e610a07d5b3282b8f19b7fc88e3b74d8
yunyu950908 <yunyu950908@gmail.com>
2017-08-22 00:06:08 +0800

    defaultvalue => value

--- src/App.js
+++ src/App.js
@@ -23,18 +23,27 @@ class App extends Component {
                 </li>
             )
         })
-        console.log(todos)
         return (
             <div className="App">
                 <h1>我的代办</h1>
                 <div className="inputWrapper">
-                    <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
+                    <TodoInput
+                        content={this.state.newTodo}
+                        onSubmit={this.addTodo.bind(this)}
+                        onChange={this.changeTitle.bind(this)}/>
                 </div>
                 <ol>
                     {todos}
                 </ol>
             </div>
         );
+    }
+
+    changeTitle(event) {
+        this.setState({
+            newTodo: event.target.value,
+            todoList: this.state.todoList
+        })
     }
 
     addTodo(event) {
