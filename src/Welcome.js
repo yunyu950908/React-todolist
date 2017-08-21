1662216cd605455290a32868b28165da9cc45ea7
yunyu950908 <yunyu950908@gmail.com>
2017-08-21 16:08:17 +0800

    想知道能在哪些钩子里 setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,34 +3,73 @@ class Welcome extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: new Date()
+            date: new Date(),
+            test: '1'
         }
-        setInterval(() => {
-            this.setState({
-                date: new Date()
-            })
+        this.setState({
+            date: new Date(),
+            test: 'constructor'
         })
-        console.log("已在 constructor 里将 props 和 state 初始化好了")
+        console.log("这里是constructor，props 和 state 初始化好了")
     }
 
     componentWillMount() {
+        this.setState({
+            date: new Date(),
+            test: 'componentWillMount'
+        })
         console.log("这里是componentWillMount，说明马上就要运行 render")
     }
 
 
     render() {
+        this.setState({
+            date: new Date(),
+            test: 'render'
+        })
         console.log("这里是render")
         return (
             <div>
                 <h1>hello,{this.props.name}</h1>
                 <h2>{this.state.date.toString()}</h2>
+                <p>{this.state.test}</p>
             </div>
         );
     }
 
     componentDidMount() {
+        this.setState({
+            date: new Date(),
+            test: 'componentDidMount'
+        })
         console.log("这里是componentDidMount, 已经挂载到页面")
     }
+
+    componentWillReceiveProps() {
+        this.setState({
+            date: new Date(),
+            test: 'componentWillReceiveProps'
+        })
+    }
+
+    shouldComponentUpdate() {
+        this.setState({
+            date: new Date(),
+            test: 'shouldComponentUpdate'
+        })
+    }
+
+    componentWillUpdate() {
+        this.setState({
+            date: new Date(),
+            test: 'componentWillUpdate'
+        })
+    }
+
+    componentWillUnmount() {
+        console.log('这里是componentWillUnmount 要死咯')
+    }
+
 }
 
 export default Welcome // 为什么要 export，为什么要加 default
