fefbc9880119f6536a9d2c41e6e201e3e41fe56d
yunyu950908 <yunyu950908@gmail.com>
2017-08-27 21:11:21 +0800

    step1 complete

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import * as localStore from './localStorage';
 // Component App
 class App extends Component {
     constructor(props) {
-        // 子类必须在constructor方法中调用super方法，否则新建实例时会报错。这是因为子类没有自己的this对象，而是继承父类的this对象，然后对其进行加工。如果不调用super方法，子类就得不到this对象。
         super(props)
         // 存储数据与状态，载入localStorage
         this.state = {
