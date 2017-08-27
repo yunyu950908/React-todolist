7751df89e4dffe5b6c59794b102d1347f0666742
yunyu950908 <yunyu950908@gmail.com>
2017-08-27 22:07:50 +0800

    cp leancloud init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import * as localStore from './localStorage';
+// leancloud-storage
+import AV from 'leancloud-storage'
+// leancloud init
+var APP_ID = 'qF7pESJt7MUOwSKvPj97SMAk-gzGzoHsz';
+var APP_KEY = 'jiE1lqfiqefFepn4zRbggNq6';
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+// leancloud ping
+var TestObject = AV.Object.extend('TestObject')
+var testObject = new TestObject()
+testObject.save({
+    words: 'Hello World!'
+}).then(function (object) {
+    alert('LeanCloud Rocks!')
+})
 
 
 // Component App
