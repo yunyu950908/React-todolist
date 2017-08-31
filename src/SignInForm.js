275d9da4d480bb951fdc35ab723cee36e75dc037
yunyu950908 <yunyu950908@gmail.com>
2017-08-31 16:30:05 +0800

    SignInForm SignUpForm改成纯函数

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -1,31 +1,29 @@
-import React, {Component} from "react";
-
-export default class SignInForm extends Component {
-    render() {
-        return (
-            <form className="signIn"
-                  onSubmit={this.props.onSubmit}>
-                <div className="row">
-                    <label htmlFor="">用户名</label>
-                    <input type="text"
-                           placeholder="username"
-                           value={this.props.formData.username}
-                           onChange={this.props.onChange.bind(null, "username")}/>
-                </div>
-                <div className="row">
-                    <label htmlFor="">密码</label>
-                    <input type="password"
-                           placeholder="password"
-                           value={this.props.formData.password}
-                           onChange={this.props.onChange.bind(null, "password")}/>
-                </div>
-                <div className="row action">
-                    <button type="submit">登录</button>
-                    <a href="javascript:;" onClick={this.props.onForgetPassword}>忘记密码？</a>
-                </div>
-            </form>
-        )
-    }
+import React from 'react';
+//登录组件
+export default function (props) {
+    return (
+        <form className="signIn"
+              onSubmit={props.onSubmit.bind(this)}>
+            <div className="row">
+                <label htmlFor="">用户名</label>
+                <input type="text"
+                       placeholder="username"
+                       value={props.formData.username}
+                       onChange={props.onChange.bind(null, "username")}/>
+            </div>
+            <div className="row">
+                <label htmlFor="">密码</label>
+                <input type="password"
+                       placeholder="password"
+                       value={props.formData.password}
+                       onChange={props.onChange.bind(null, "password")}/>
+            </div>
+            <div className="row action">
+                <button type="submit">登录</button>
+                <a href="javascript:;" onClick={props.onForgetPassword}>忘记密码？</a>
+            </div>
+        </form>
+    )
 }
 
 
--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,36 +1,36 @@
-import React, {Component} from "react";
-
+import React from 'react';
 // 注册组件
-export default class SignUpForm extends Component {
-    render() {
-        return (
-            <form className="signUp"
-                  onSubmit={this.props.onSubmit.bind(this)}>
-                <div className="row">
-                    <label htmlFor="">邮箱</label>
-                    <input type="email"
-                           placeholder="email address"
-                           value={this.props.formData.email}
-                           onChange={this.props.onChange.bind(null, "email")}/>
-                </div>
-                <div className="row">
-                    <label htmlFor="">用户名</label>
-                    <input type="text"
-                           placeholder="username"
-                           value={this.props.formData.username}
-                           onChange={this.props.onChange.bind(null, "username")}/>
-                </div>
-                <div className="row">
-                    <label htmlFor="">密码</label>
-                    <input type="password"
-                           placeholder="password"
-                           value={this.props.formData.password}
-                           onChange={this.props.onChange.bind(null, "password")}/>
-                </div>
-                <div className="row action">
-                    <button type="submit">注册</button>
-                </div>
-            </form>
-        )
-    }
+export default function (props) {
+    return (
+        <form className="signUp"
+              onSubmit={props.onSubmit.bind(this)}>
+            <div className="row">
+                <label htmlFor="">邮箱</label>
+                <input type="email"
+                       placeholder="email address"
+                       value={props.formData.email}
+                       onChange={props.onChange.bind(null, "email")}/>
+            </div>
+            <div className="row">
+                <label htmlFor="">用户名</label>
+                <input type="text"
+                       placeholder="username"
+                       value={props.formData.username}
+                       onChange={props.onChange.bind(null, "username")}/>
+            </div>
+            <div className="row">
+                <label htmlFor="">密码</label>
+                <input type="password"
+                       placeholder="password"
+                       value={props.formData.password}
+                       onChange={props.onChange.bind(null, "password")}/>
+            </div>
+            <div className="row action">
+                <button type="submit">注册</button>
+            </div>
+        </form>
+    )
 }
+
+
+
